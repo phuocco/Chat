730169ed0b8b81c9fbe46d1b407caefcd2a0bd0d
phuocco <vodanh.2901@gmail.com>
2019-05-23 19:12:27 +0700

    video 5

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.chat;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
-   Toolbar mToolbar;
+ private   Toolbar mToolbar;
+ private ViewPager myViewPager;
+ private TabLayout myTabLayout;
+ private TabAccessorAdapter myTabAccessorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mToolbar =(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("AppChat");
+
+        myViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabAccessorAdapter = new TabAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessorAdapter);
+
+        myTabLayout =(TabLayout)findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
     }
 }
