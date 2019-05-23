58a2976fe1c415f6f0687ed7dce2bc59a979fee5
phuocco <vodanh.2901@gmail.com>
2019-05-23 18:01:08 +0700

    video 4

---
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+   Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar =(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("AppChat");
+    }
+}
