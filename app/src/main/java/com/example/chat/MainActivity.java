64868a8bd7f56d10b585b703602d47590066ab97
phuocco <vodanh.2901@gmail.com>
2019-05-28 12:37:45 +0700

    11 logout

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
  private TabAccessorAdapter myTabAccessorAdapter;
 
  private FirebaseUser currentUser;
-
+private FirebaseAuth mAuth;
 
 
     @Override
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
         mToolbar =(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("AppChat");
@@ -52,5 +57,31 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if(item.getItemId()==R.id.main_logout_option)
+        {
+                mAuth.signOut();
+                SendUserToLoginActivity();
+        }
+        if(item.getItemId()==R.id.main_settings_option)
+        {
+
+        }
+        if(item.getItemId()==R.id.main_find_friends_option)
+        {
+
+        }
+        return true;
     }
 }
